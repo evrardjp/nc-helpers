216ed579d9d47885b5ba2ffeac89ed5c1f47b2af
Pete Birley <petebirley+github@gmail.com>
2019-06-11 16:47:00 -0500

    Update things.sh

--- things.sh
+++ things.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-function get_kubectl {
-  CONTAINER=$(docker create "$(awk '/hyperkube-amd64/ { print $1 ; exit }' /usr/local/bin/kubectl)")
-  docker cp "$CONTAINER:/hyperkube" "$HOME/kubectl"
-  docker rm "$CONTAINER"
-}
-
 function kubectl {
   export KUBECONFIG=/etc/kubernetes/admin/kubeconfig.yaml
+  if [ ! -f $HOME/kubectl ]; then
+    CONTAINER=$(docker create "$(awk '/hyperkube-amd64/ { print $1 ; exit }' /usr/local/bin/kubectl)")
+    docker cp "$CONTAINER:/hyperkube" "$HOME/kubectl"
+    docker rm "$CONTAINER"
+  fi
   $HOME/kubectl $@
+}
+
+function helm {
+  export KUBECONFIG=/etc/kubernetes/admin/kubeconfig.yaml
+  if [ ! -f $HOME/helm ]; then
+    CONTAINER=$(docker create "$(awk '/k8s-helm/ { print $1 ; exit }' /usr/local/bin/helm)")
+    docker cp "$CONTAINER:/usr/local/bin/helm" "$HOME/helm"
+    docker rm "$CONTAINER"
+  fi
+  $HOME/helm $@
 }
 
 function host_jump {
   HOST=$1
-  kubectl -n ucp exec -it "$(kubectl get -n ucp pods -l application=divingbell,component=sysctl -o wide --no-headers | awk "/${HOST}/ { print \$1; exit }")" -- nsenter -t 1 -m -u -n -i bash
+  kubectl -n ucp exec -it "$(kubectl get -n ucp pods -l application=divingbell,component=exec -o wide --no-headers | awk "/${HOST}/ { print \$1; exit }")" -- nsenter -t 1 -m -u -n -i bash
 }
 
 function get_openrc {
   SERVICE=$1
   USER=$2
-  kubectl get -n openstack secrets "${SERVICE}-keystone-${USER}" -o go-template='{{ range $k, $v := .data }}export {{$k}}={{ $v | base64decode}}{{"\n"}}{{end}}'
+  kubectl get -n openstack secrets "${SERVICE}-keystone-${USER}" -o go-template --template='{{ range $k, $v := .data }}export {{$k}}={{ $v | base64decode}}{{"\n"}}{{end}}'
   echo "export OS_IDENTITY_API_VERSION=3"
 }
 
 function get_config {
   NS=$1
   SECRET=$2
-  kubectl get -n "$NS" secrets "${SECRET}" -o go-template='{{ range $k, $v := .data }}--- {{$k}} ---{{"\n"}}{{ $v | base64decode }}{{"\n"}}{{end}}'
+  kubectl get -n "$NS" secrets "${SECRET}" -o go-template --template='{{ range $k, $v := .data }}--- {{$k}} ---{{"\n"}}{{ $v | base64decode }}{{"\n"}}{{end}}'
 }
 
 function job_rerun {

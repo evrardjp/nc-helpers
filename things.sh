9d30f0ad63e9b7e495c73ea38ae335f62bf20a15
Pete Birley <petebirley+github@gmail.com>
2019-06-12 12:42:17 -0500

    Update things.sh

--- things.sh
+++ things.sh
@@ -28,9 +28,13 @@ function host_jump {
 function get_openrc {
   SERVICE=$1
   USER=$2
-  kubectl get -n openstack secrets "${SERVICE}-keystone-${USER}" -o go-template --template='{{ range $k, $v := .data }}export {{$k}}={{ $v | base64decode}}{{"\n"}}{{end}}'
+  TEMPLATE=$(mktemp)
+  echo '{{ range $k, $v := .data }}export {{$k}}={{ $v | base64decode}}{{"\n"}}{{end}}' > ${TEMPLATE}
+  kubectl get -n openstack secrets "${SERVICE}-keystone-${USER}" -o go-template-file=${TEMPLATE}
+  rm -f ${TEMPLATE}
   echo "export OS_IDENTITY_API_VERSION=3"
 }
+
 
 function get_config {
   NS=$1
